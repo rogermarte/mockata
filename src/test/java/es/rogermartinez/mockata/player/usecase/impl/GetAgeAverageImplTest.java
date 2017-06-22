85d2068dea254506b966f2e2ad7aa8f068da91fe
Roger Martínez <rmartinez@stacks-cis.ct>
2017-06-22 11:42:36 +0200

    Third test

--- src/test/java/es/rogermartinez/mockata/player/usecase/impl/GetAgeAverageImplTest.java
+++ src/test/java/es/rogermartinez/mockata/player/usecase/impl/GetAgeAverageImplTest.java
@@ -1,10 +1,13 @@
 package es.rogermartinez.mockata.player.usecase.impl;
 
+import es.rogermartinez.mockata.player.Player;
 import es.rogermartinez.mockata.player.exception.NoPlayerFoundException;
 import es.rogermartinez.mockata.player.repository.PlayerRepository;
 import es.rogermartinez.mockata.player.repository.impl.PlayerInMemory;
 import es.rogermartinez.mockata.player.usecase.GetAgeAverage;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertTrue;
 import static org.mockito.BDDMockito.given;
@@ -29,6 +32,18 @@ public class GetAgeAverageImplTest {
         // Given
         PlayerRepository playerRepositoryMock = mock(PlayerRepository.class);
         given(playerRepositoryMock.obtainAllPlayers()).willReturn(null);
+        GetAgeAverage getAgeAverage = new GetAgeAverageImpl(playerRepositoryMock);
+        // When
+        getAgeAverage.calculate();
+        // Then
+        assertTrue(false);
+    }
+
+    @Test(expected = NoPlayerFoundException.class)
+    public void should_throws_no_player_exception_when_repository_return_no_players() throws Exception {
+        // Given
+        PlayerRepository playerRepositoryMock = mock(PlayerRepository.class);
+        given(playerRepositoryMock.obtainAllPlayers()).willReturn(new ArrayList<Player>());
         GetAgeAverage getAgeAverage = new GetAgeAverageImpl(playerRepositoryMock);
         // When
         getAgeAverage.calculate();
